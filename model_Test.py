f33e91b5dedf0f8f19de7b9b6b7928f1487de268
Nqile <87355946+Nqile@users.noreply.github.com>
2025-02-06 11:39:39 +0800

    Update model_Test.py

--- model_Test.py
+++ model_Test.py
@@ -17,26 +17,46 @@ def test_other_east():
     model_no_move = GameModel([[None, None],[1, 2]])
     model_no_move_1 = GameModel([[None, 1],[1, 2]])
     model_no_move.order("E")
-
     assert model_no_move == model_no_move_1
+
+    model_rest = GameModel([[None, None, None], [2, 2, 3]])
+    model_rest_1 = GameModel([[None, None, None], [1, 3, 3]])
+    model_rest.order("E")
+    assert model_rest == model_rest_1
+
 
 def test_other_south():
     model_no_move = GameModel([[None, 1],[None, 2]])
     model_no_move_1 = GameModel([[None, 1],[1, 2]])
     model_no_move.order("S")
-
     assert model_no_move == model_no_move_1
+
+    model_rest = GameModel([[None, None, 2], [None, None, 2], [None, None, 3]])
+    model_rest_1 = GameModel([[None, None, None], [None, None, 3], [None, 1, 3]])
+    model_rest.order("S")
+    assert model_rest == model_rest_1
+
+
 
 def test_other_west():
     model_no_move = GameModel([[None, None],[1, 2]])
     model_no_move_1 = GameModel([[None, 1],[1, 2]])
     model_no_move.order("W")
-
     assert model_no_move == model_no_move_1
+
+    model_rest = GameModel([[None, None, None], [3, 2, 2]])
+    model_rest_1 = GameModel([[None, None, None], [3, 3, 1]])
+    model_rest.order("W")
+    assert model_rest == model_rest_1
+    
 
 def test_other_north():
     model_no_move = GameModel([[None, 1],[None, 2]])
     model_no_move_1 = GameModel([[None, 1],[1, 2]])
     model_no_move.order("N")
-
     assert model_no_move == model_no_move_1
+
+    model_rest = GameModel([[None, None, 3], [None, None, 2], [None, None, 2]])
+    model_rest_1 = GameModel([[None, None, 3], [None, None, 3], [None, None, 1]])
+    model_rest.order("N")
+    assert model_rest == model_rest_1
